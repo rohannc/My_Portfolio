f2ff2c49cf56528268695b4c8e5a4d08cf78d3bb
Rohan Chakraborty <chakrabortyrohan.abc01@gmail.com>
2025-05-02 16:03:12 +0530

    Rechecking the path

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,7 +2,7 @@
 
 import { ref } from "vue";
 
-const imageUrl = ref("../assets/ProfileImageCropped.jpg");
+const imageUrl = ref("../../assets/ProfileImageCropped.jpg");
 const designationLine1 = ref("Rohan Chakraborty");
 const designationLine2 = ref("Computer Science Graduate || Software Developer || AI Enthusiast");
 const textContent = ref("I’m an MCA student at Jadavpur University, with a love for cracking tough problems through code. My journey with Computer started at Bangabasi College, and now I’m diving deep into tech. I built a DeepFake detection tool with my team that hit 99.98% accuracy using Python and TensorFlow. I thrive in the buzz of hackathons like DoubleSlash 3.0 and coding contests like CodeCrafter, having tackled over 450 problems on LeetCode and GeeksforGeeks and other platforms. I’m always curious, always creating, and excited for what’s next—can’t wait to share more!");
@@ -41,7 +41,7 @@ const downloadResume = () => {
         <div class="part2">
             <div class="pt-10 w-full rounded-lg overflow-hidden text-center pl-10">
                 <div class="w-full h-80 overflow-hidden">
-                    <img :src="imageUrl" class="w-full h-full object-cover rounded-full" alt="Profile Image">
+                    <img src="../assets/ProfileImageCropped.jpg" class="w-full h-full object-cover rounded-full" alt="Profile Image">
                 </div>
                 <div class="p-2 pt-4">
                     <p class="m-0 text-3xl pt-serif-bold-italic font-semibold text-gray-300">{{ designationLine1 }}
