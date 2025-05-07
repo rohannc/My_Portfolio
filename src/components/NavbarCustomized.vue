4635e1dc1ca5e5163df525f45752ce1ddb9dc4b4
rohannc <chakrabortyrohan.abc01@gmail.com>
2025-05-07 22:39:40 +0530

    Navbar likely to be done

--- src/components/NavbarCustomized.vue
+++ src/components/NavbarCustomized.vue
@@ -83,16 +83,19 @@ onMounted(() => {
 
 <template>
     <nav id="navbar" class="fixed top-0 w-full z-50 bg-white border-gray-200 dark:bg-gray-900">
-        <div class="w-[90%] mx-auto flex items-center justify-between p-4">
+        <div class="w-[91%] ml-[5%] mr-[4%] flex items-center justify-between p-4">
             <!-- Logo Section -->
             <a href="#" class="flex items-center space-x-3">
                 <img class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
                     src="../assets/ProfileImageCropped.jpg" alt="Bordered avatar">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white hidden md:block">
+                    Rohan Chakraborty
+                </span>
             </a>
             <!-- Navigation and Toggle -->
             <div class="flex items-center space-x-8">
                 <!-- Desktop Navigation -->
-                <ul class="hidden md:flex space-x-8 font-medium ml-auto">
+                <ul class="hidden md:flex space-x-8 font-medium">
                     <li v-for="(item, index) in navItems" :key="index">
                         <a :href="item.url" @click="handleNavClick(item)" class="inline-flex items-center space-x-2"
                             :class="[
@@ -112,7 +115,7 @@ onMounted(() => {
                                     d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998a12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                                 <path v-if="item.icon === 'trophy'" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2"
-                                    d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3. LEDs to be 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                                    d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
                                 <path v-if="item.icon === 'briefcase'" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2"
                                     d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
@@ -125,7 +128,7 @@ onMounted(() => {
                     </li>
                 </ul>
                 <!-- Dark Mode Toggle -->
-                <label class="switch md:ml-4 mb-1.5">
+                <label class="switch md:ml-4 mb-1.5 mr-8">
                     <input type="checkbox" v-model="isDarkMode" @click="toggleDarkMode">
                     <span class="slider"></span>
                 </label>
@@ -169,7 +172,7 @@ onMounted(() => {
                                 stroke-width="2"
                                 d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                         </svg>
-                        <span>{{ item.name }}</span>
+                        <span class="md:hidden">{{ item.name }}</span>
                     </a>
                 </li>
             </ul>
